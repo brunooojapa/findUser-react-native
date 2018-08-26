4ea2aa40f62aa424e37feb74384c13968c6bf21d
brunooojapa <brunooojapa@gmail.com>
2018-08-26 13:24:42 -0300

    :bento: add font && config app.js

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RootNavigation from './navigation/RootNavigator';
+import { AppLoading, Asset, Font } from 'expo';
 
 export default class App extends React.Component {
+	state = {
+		fontLoaded: false
+	};
+	async componentDidMount() {
+		await Font.loadAsync({
+			'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf')
+		});
+		// await Asset.loadAsync([
+		// 	require('./assets/images/background_login.png'),
+		// 	require('./assets/images/logo_drawer.png')
+		// ]);
+	}
 	render() {
+		if (!this.state.assestsLoad) {
+			return (
+				<AppLoading
+					startAsync={this.componentDidMount}
+					onFinish={() => this.setState({ assestsLoad: true })}
+					onError={console.warn}
+				/>
+			);
+		}
 		return (
 			<View style={styles.container}>
-				<Text> Open up App.js to start working on your app! </Text>
-				<Text> Changes you make will automatically reload. </Text>
-				<Text> Shake your phone to open the developer menu. </Text>
+				{this.state.assestsLoad ? (
+					<RootNavigation style={{ fontFamily: 'Lato-Regular' }} />
+				) : null}
 			</View>
 		);
 	}
@@ -16,8 +39,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'blue',
-		alignItems: 'center',
+		backgroundColor: '#fff',
 		justifyContent: 'center'
+	},
+	statusBarUnderlay: {
+		height: 24,
+		backgroundColor: 'rgba(0,0,0,0.2)'
+	},
+	btn: {
+		backgroundColor: '#fff'
 	}
 });
